74de892ab75e94e223b4fc06cf9b5aed2c257264
FedericoPavesi <federico.pavesiwork@gmail.com>
2024-02-13 10:27:37 +0100

    fix setup

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     name='VAENAS',
     version=__version__,
 
-    url='https://github.com/fpavesi11/AutoPrune',
+    url='https://github.com/fpavesi11/NASVAE',
     author='Federico Pavesi',
     author_email='f.pavesi11@campus.unimib.it',
 
