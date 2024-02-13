e654370d8977017d2765f3b79259d3f87cebed9a
FedericoPavesi <federico.pavesiwork@gmail.com>
2024-02-13 10:12:52 +0100

    First commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+from __version__ import __version__
+
+setup(
+    name='VAENAS',
+    version=__version__,
+
+    url='https://github.com/fpavesi11/AutoPrune',
+    author='Federico Pavesi',
+    author_email='f.pavesi11@campus.unimib.it',
+
+    packages=find_packages(),
+)
